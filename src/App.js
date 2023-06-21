bb6480cce89c571d3aebfaf0e5734f742cb66965
SahilMangla14 <108575832+SahilMangla14@users.noreply.github.com>
2023-06-21 05:49:31 +0530

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Slider from './components/Slider';
+import Topbar from './components/Topbar';
+import Home from './components/Home';
+import About from './components/About'
+import Message from './components/Message';
+import Hostel from './components/Hostel';
+import Gallery from './components/Gallery';
+import Events from './components/Events';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar />
+      <Home />
+      <About />
+      <Message/>
+      <Hostel/>
+      <Events />
+      <Gallery />
+      <Footer/>
     </div>
   );
 }
